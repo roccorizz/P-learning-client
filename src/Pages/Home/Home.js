23c68594dd26f72164f119d2e00377e46ae12ebf
roccorizz <sknahid68@gmail.com>
2022-10-27 19:12:33 +0600

    header correction

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import slider1 from './slider1.jpg';
+import slider2 from './slider2.jpg';
+import slider3 from './slider3.jpg';
+import './Home.css';
+import { useLoaderData } from 'react-router-dom';
+
+import { FaGraduationCap, FaChalkboardTeacher } from "react-icons/fa";
+import SingleCategory from '../SingleCategory/SingleCategory';
+
+const Home = () => {
+    const allcategories = useLoaderData();
+
+    return (
+
+        <div>
+            <div className="mb-5">
+                <AwesomeSlider className="slider">
+                    <div data-src={slider2} />
+                    <div data-src={slider3} />
+                    <div data-src={slider1} />
+                </AwesomeSlider>
+            </div>
+            <div className='container'>
+                <div className="about">
+                    <div className='row'>
+                        <div className='col-md-7 col-sm-12 mt-5'>
+                            <h2>TryCatch | Best IT Training Institute in Bangladesh</h2>
+                            <p>TryCatch is recognized as one of Bangladesh's best global skill development and IT training centres. The journey of TryCatch started in 2014 with a big dream to bring applied education to the mass population at an affordable cost. The main purpose of TryCatch is to facilitate the underprivileged, disadvantaged people, especially youth and women, with the best IT-related skills.</p>
+
+                        </div>
+                        <div className='col-md-5 col-sm-12 mt-5 text-light'>
+                            <div className="d-flex justify-content-between">
+
+                                <div className='card text-center'>
+                                    <div className='card-tittle'>
+                                        <FaGraduationCap className="fs-1 mt-3"></FaGraduationCap>
+                                    </div>
+                                    <div className="card-body">
+                                        <h3>10K+</h3>
+                                        <h6 className="w-100 mt-3">Graduate Students</h6>
+                                    </div>
+
+                                </div>
+                                <div className='card text-center'>
+                                    <div className='card-tittle'>
+                                        <FaChalkboardTeacher className="fs-1 mt-3"></FaChalkboardTeacher>
+                                    </div>
+                                    <div className="card-body">
+                                        <h3>200+</h3>
+                                        <h6 className="mt-3">Expert Mentors</h6>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="departments">
+                <div className='container'>
+                    <h2 className="text-center mt-5">Departments</h2>
+
+                    <div className="row">
+                        {
+                            allcategories.map((category) =>
+
+                                <SingleCategory key={category.id} category={category}></SingleCategory>
+                            )
+                        }
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,28 +1,70 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaUserAlt } from "react-icons/fa";
+
+
+import { NavLink } from 'react-router-dom';
+import logo from './logo.png';
+import Image from 'react-bootstrap/Image';
+import './Header.css';
+import { AuthContext } from '../../../Context/AuthProvider';
 
 const Header = () => {
+    const { user, SignOut } = useContext(AuthContext);
+    const logOut = () => {
+        SignOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Plearning</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#courses">Courses</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link to='/about'>About us</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#1E2F50" }}>
+                <Container>
+                    <Navbar.Brand className="d-flex align-items-center">
+                        <Image src={logo} style={{ width: "100px", marginTop: "-10px" }}></Image>
+                        <h3 className="text-light" style={{ paddingTop: "15px" }}>TryCatch</h3>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav className="ms-auto nav-item">
+
+                            <NavLink to="/courses" activeclassname="active">COURSES</NavLink>
+                            <NavLink to="/faq" activeclassname="active">FAQ</NavLink>
+                            <NavLink to="/blog" activeclassname="active">BLOG</NavLink>
+
+
+                            {
+                                user?.uid ?
+                                    <>
+                                        {user?.photoURL ?
+                                            <>
+                                                <abbr title={user.displayName}> <Image src={user?.photoURL}
+                                                    style={{ height: '30px' }}
+                                                    roundedCircle></Image></abbr>
+                                            </> :
+                                            <>
+                                                <FaUserAlt className='fs-2 text-white'></FaUserAlt>
+                                            </>
+
+                                        }
+                                        <button className='btn btn-danger btn-sm ms-2' onClick={logOut}>LogOut</button>
+                                    </>
+                                    :
+                                    <>
+                                        <NavLink to="/login" activeclassname="active">LOGIN</NavLink>
+                                        <NavLink to="/register" activeclassname="active">REGISTER</NavLink>
+                                    </>
+                            }
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
